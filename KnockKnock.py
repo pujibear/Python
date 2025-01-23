1afa961452ae9935a44aa8eafb877e071d7e8de0
pujibear <bcruz@students.mchenry.edu>
2025-01-22 18:40:23 -0600

    Add files via upload

---
+++ KnockKnock.py
@@ -0,0 +1,12 @@
+# Dark knock knock joke i found on reddit
+
+print("Knock, Knock..")
+print("Whose there?")
+print("Whose there..")
+print("Oh thats right I dont have a house....")
+#Nico is a friend of mine while i was doing the hw
+print()
+print("Knock, Knock..","Whose there?","Whose there..","Oh thats right I dont have a house....",sep=" N1C0 ")
+
+print()
+print("Knock, Knock..\nWhose there?\nWhose there..\nOh thats right I dont have a house....")
