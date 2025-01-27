2f06f8fb154dc6df9a408d43362c77b78932ecab
pujibear <bcruz@students.mchenry.edu>
2025-01-27 16:44:23 -0600

    Add files via upload

---
+++ Data_types.py
@@ -0,0 +1,28 @@
+#
+#I changed up the variables? i tried my own thing on the bottom. let me know if that was too much or something.
+animal="Chavez"
+
+x = 45
+print("Literal:", x)
+
+
+y = -15
+print("Integer:", y)
+
+
+z = 2.1
+print("Float:", z)
+
+
+name = "Bryan"
+print("String:", name)
+
+
+is_active = True
+print("Boolean:", is_active)
+
+print("My name is",name , "and I am", x)
+print(animal,"is my dog")
+print("my Highschool gpa was", z)
+print()
+print("My name is", name,"\n",animal, "is my dog", "\n","My highscool gpa was",z, sep=(" "))
